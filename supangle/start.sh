d2795eec231fef5110f59d869dd11aa48f78c90f
lareii <lareii@proton.me>
2024-03-12 00:15:34 +0300

    add: superuser privilege check

--- supangle/start.sh
+++ supangle/start.sh
@@ -1,3 +1,8 @@
+if [ "$EUID" -ne 0 ]
+then 
+  echo "Please run with superuser privileges."
+  exit
+fi
 
 if ! command -v "nodemon" /dev/null
 then
@@ -6,8 +11,16 @@ fi
 
 if ! command -v "ccache" /dev/null
 then
-  sudo apt-get install -y ccache
+  source /etc/os-release
+  if [ "$ID" = "ubuntu" ]
+  then
+    apt-get update
+    apt-get install -y ccache
+  fi
+  if [ "$ID" = "fedora" ]
+  then
+    dnf install -y ccache
+  fi
 fi
-
 
 nodemon -e cc,h,js,cpp,hpp --exec "make && ./bin/supangle index.js"
